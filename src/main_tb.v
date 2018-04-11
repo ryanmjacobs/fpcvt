8aa65fb518063c1ab978a13a12aaf9a8b48aa9d4
Ryan Jacobs <ryan@rmj.us>
2018-04-11 00:51:35 -0700

    add expansion

--- src/main_tb.v
+++ src/main_tb.v
@@ -6,13 +6,17 @@ wire  [7:0] out;
 main m(in, out);
 
 integer x;
+integer expanded;
+
 initial begin
     // Rounding Examples 1-4
     for (x = 44; x <= 47; x=x+1) begin
-        in = 125; #5
+        in = 422; #5
+
+        expanded = (-1)**out[7] * out[3:0] * 2**out[6:4];
+
         $display("in  = %b (%0d)", in, in);
-        $display("out = %12b (%0d)", out,
-            (-1)**out[7] * out[3:0] * 2**out[6:4]);
+        $display("out = %12b (%0d)", out, expanded);
         $display("%0d,%0d,%0d", out[7], out[3:0], out[6:4]);
         $display("");
     end
