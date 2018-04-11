cbf095c6d9137e95a3d9d50bcbc0767d37d158f6
Ryan Jacobs <ryan@rmj.us>
2018-04-11 01:44:56 -0700

    add waveform

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fpcvt (CS 152A) ![build status](https://travis-ci.org/ryanmjacobs/fpcvt.svg?branch=master)
+
+![waveform](https://github.com/ryanmjacobs/fpcvt/raw/master/waveform.png)
--- src/main_tb.v
+++ src/main_tb.v
@@ -13,10 +13,12 @@ wire [3:0] val  = out[3:0];
 integer x, expanded;
 
 initial begin
+    $dumpfile("main_tb.vcd");
+    $dumpvars(0, in, out);
+
     // Rounding Examples 1-4
     for (x = 44; x <= 47; x=x+1) begin
-        in = x; #5
-
+        in = x; #1
         expanded = (-1)**sign * val * 2**exp;
 
         $display("in  = %b (%0d)", in, in);
