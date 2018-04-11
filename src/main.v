5250004dacf96eae223eded5b27c3addfb1b2791
Ryan Jacobs <ryan@rmj.us>
2018-04-10 23:23:19 -0700

    wip main (without rounding yet)

--- src/main.v
+++ src/main.v
@@ -1,20 +1,19 @@
-module counter(out, clk, reset);
+module main(
+    input  [11:0] in, // linear encoding (two's complement)
+    output [7:0]  out // floating point output
+);
 
-  parameter WIDTH = 8;
+wire sign;
+wire [10:0] magnitude;
+wire  [2:0] exponent;
+wire  [3:0] significand;
 
-  output [WIDTH-1 : 0] out;
-  input 	       clk, reset;
+  TCTSM a(in, sign, magnitude);
+convert b(magnitude, exponent, significand, fifth_bit);
+//round c(exponent, significand, fifth_bit);
 
-  reg [WIDTH-1 : 0]   out;
-  wire 	       clk, reset;
+assign out[7]   = sign;
+assign out[6:4] = exponent;
+assign out[3:0] = significand;
 
-  always @(posedge clk)
-    out <= out + 1;
-
-  always @reset
-    if (reset)
-      assign out = 0;
-    else
-      deassign out;
-
-endmodule // counter
+endmodule
--- src/main_tb.v
+++ src/main_tb.v
@@ -1,25 +1,14 @@
 module test;
-  /* Make a reset that pulses once. */
-  reg reset = 0;
-  initial begin
-     $dumpfile("test.vcd");
-     $dumpvars(0,test);
-  
-     # 17 reset = 1;
-     # 11 reset = 0;
-     # 29 reset = 1;
-     # 11 reset = 0;
-     # 100 $stop;
-  end
-  
-  /* Make a regular pulsing clock. */
-  reg clk = 0;
-  always #5 clk = !clk;
-  
-  wire [7:0] value;
-  counter c1 (value, clk, reset);
-  
-  initial
-     $monitor("At time %t, value = %h (%0d)",
-              $time, value, value);
-endmodule // test
+
+reg  [11:0] in = 422;
+wire  [7:0] out;
+
+main m(in, out);
+
+initial begin
+   #10
+   $display("in  = %b (%0d)", in, in);
+   $display("out = %b (%0d)", out, out);
+end
+
+endmodule
