6dd0c7f2c420b107df325031f28eb7d74472bf27
Ryan Jacobs <ryan@rmj.us>
2018-04-10 22:53:26 -0700

    correctly calc exp

--- src/convert.v
+++ src/convert.v
@@ -7,13 +7,14 @@ module convert(
 
 assign significand = (magnitude);
 
-assign exponent =
+assign exponent = 8 -
    (magnitude[10] ? 0 :
     magnitude[9]  ? 1 :
     magnitude[8]  ? 2 :
     magnitude[7]  ? 3 :
     magnitude[6]  ? 4 :
     magnitude[5]  ? 5 :
-    magnitude[4]  ? 6 : 7);
+    magnitude[4]  ? 6 :
+    magnitude[3]  ? 7 : 8);
 
 endmodule
